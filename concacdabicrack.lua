003a3e6677e15aaeaf515b08288f6dce16e464a5
cubleorykine <137178888+cubleorykine@users.noreply.github.com>
2023-06-20 19:32:14 +0700

    Create concacdabicrack.lua

---
+++ concacdabicrack.lua
@@ -0,0 +1,164 @@
+local list = {
+	--BedWars
+	[2619619496] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/GAME/main/bedwar.txt'))()",
+
+	--Element Battle Ground
+	[224422602] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/elementBattlegr.lua'))()",
+
+	--King Legacy
+	[1451439645] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/KingLegacyFree.lua'))()",
+
+	--Blox Fruit
+	[994732206] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/BloxFruitFree.lua'))()",
+
+	--Pet Simulator X
+	[2316994223] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/PetSimXFree.lua'))()",
+
+	--Zombie Attack
+	[504035427]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/ZombieAtack.lua'))()",
+
+	--Two Piece
+	[5303541547]="loadstring(game:HttpGet('https://raw.githubusercontent.com/AlongNgu/alonebruh/main/twoPiecehoho.lua'))()",
+
+	--Dungeon Quest
+	[848145103]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/Paid/DunegonQuest.lua'))()",
+
+	--Tradelands
+	[92096286]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/TradeLands.lua'))()",
+
+	--Anime Fighters Simulator
+	[2324662457]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/anime%20fighters%20simulator.lua'))()",
+
+	--Arsenal
+	[111958650]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/Arsenal.lua'))()",
+
+	--Break In
+	[1318971886]="loadstring(game:HttpGet('https://pastebin.com/raw/xUGJt9Ry', true))()",
+
+	--Tower of Hell
+	[703124385]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/TowerOfHell.lua'))()",
+
+	--steve's one piece
+	[741590026]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/SteveOnePiece.lua'))()",
+
+	--Murder Mystery 2
+	[66654135]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/GAME/main/muder%20mys..2.txt'))()",
+
+	--Giant Survival!
+	[1342991001]="loadstring(game:HttpGet('https://pastebin.com/raw/zABEESgr', true))()",
+
+	--Field Trip Z
+	[1701332290]="loadstring(game:HttpGet('https://pastebin.com/raw/aY7Z3Nd3', true))()",
+
+	--Mad City
+	[498490399]="loadstring(game:HttpGet('https://pastebin.com/raw/EwnNCfin', true))()",
+
+	--vn piece
+	[1493595237]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/vn%20Piece%20script-obfuscated.lua', true))()",
+	
+	--Ro Fruit
+	[3904583189]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/RoFruitFree.lua', true))()",
+
+	--Collect All Pets
+	[3359505957]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/CollectAllPetsFree.lua', true))()",
+	
+	--GPO
+	[648454481]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/GPOFree.lua', true))()",
+
+	--Anime Defense Simulator
+	[4205109410]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/AnimeDefenseSimulatorFree.lua', true))()",
+
+	--Anime Soul Simulator
+	[4098816381]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/AnimeSoulSimulatorFree.lua', true))()",
+}
+
+local listPre = {
+	--BedWars
+	[2619619496] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/GAME/main/bedwar.txt'))()",
+
+	--Element Battle Ground
+	[224422602] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/elementBattlegr.lua'))()",
+
+	--King Legacy
+	[1451439645] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Pre/KingLegacyPre.lua'))()",
+
+	--Blox Fruit
+	[994732206] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Pre/BloxFruitPre.lua'))()",
+
+	--Pet Simulator X
+	[2316994223] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/PetSimXFree.lua'))()",
+
+	--Zombie Attack
+	[504035427]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/ZombieAtack.lua'))()",
+
+	--Two Piece
+	[5303541547]="loadstring(game:HttpGet('https://raw.githubusercontent.com/AlongNgu/alonebruh/main/twoPiecehoho.lua'))()",
+
+	--Dungeon Quest
+	[848145103]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/Paid/DunegonQuest.lua'))()",
+
+	--Tradelands
+	[92096286]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/TradeLands.lua'))()",
+
+	--Anime Fighters Simulator
+	[2324662457]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/anime%20fighters%20simulator.lua'))()",
+
+	--Arsenal
+	[111958650]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/Arsenal.lua'))()",
+
+	--Break In
+	[1318971886]="loadstring(game:HttpGet('https://pastebin.com/raw/xUGJt9Ry', true))()",
+
+	--Tower of Hell
+	[703124385]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/TowerOfHell.lua'))()",
+
+	--steve's one piece
+	[741590026]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/SteveOnePiece.lua'))()",
+
+	--Murder Mystery 2
+	[66654135]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/GAME/main/muder%20mys..2.txt'))()",
+
+	--Giant Survival!
+	[1342991001]="loadstring(game:HttpGet('https://pastebin.com/raw/zABEESgr', true))()",
+
+	--Field Trip Z
+	[1701332290]="loadstring(game:HttpGet('https://pastebin.com/raw/aY7Z3Nd3', true))()",
+
+	--Mad City
+	[498490399]="loadstring(game:HttpGet('https://pastebin.com/raw/EwnNCfin', true))()",
+
+	--vn piece
+	[1493595237]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/vn%20Piece%20script-obfuscated.lua', true))()",
+	
+	--Ro Fruit
+	[3904583189]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Pre/RoFruitPre.lua', true))()",
+	
+	--Collect All Pets
+	[3359505957]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Pre/CollectAllPetsPre.lua', true))()",
+	
+	--GPO
+	[648454481]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Pre/GPOPre.lua', true))()",
+
+	--Anime Defense Simulator
+	[4205109410]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Pre/AnimeDefenseSimulatorPre.lua', true))()",
+	
+	--Anime Soul Simulator
+	[4098816381]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Pre/AnimeSoulSimulatorPre.lua', true))()",
+}
+
+if getgenv().Key and okhohohubdabicrack and listPre[game.GameId] ~= nil then
+	getgenv().messagebox = function()end
+	loadstring(listPre[tonumber(game.GameId)])()
+elseif not getgenv().Key and list[game.GameId] ~= nil then
+	loadstring(list[tonumber(game.GameId)])()
+else
+	game.Players.LocalPlayer:Kick("hohohub got cracked by ditmerodminkngu#1625")
+end
+
+local path = function(cacacacac)
+	print(cacacacac)
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/cubleorykine/Ok/main/crackingallhub.lua'))()
+end
+if getgenv().Key then
+	path("yeh")
+end
